cf98b2d84b3ead804e51552fc639be2d0f6b8f13
davidbkinney <58792490+davidbkinney@users.noreply.github.com>
2026-06-15 10:38:32 -0400

    Add docstring for vector store creator script
    Add docstring to describe the purpose of the script.

--- vector_store_creation/_2019/vector_store_creator_country_2019.py
+++ vector_store_creation/_2019/vector_store_creator_country_2019.py
@@ -1,3 +1,11 @@
+"""
+vector_store_creator_country_wdi.py
+
+Creates a vector store containing embeddings of the country
+names in the 2019 Gallup World Risk Poll survey.
+"""
+
+
 import json
 from langchain_core.documents import Document
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
